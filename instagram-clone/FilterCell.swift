3e22d7a92014772ce1c4e8ce7c62952b84cba445
Mac <Mac@Macui-MacBook-Air.local>
2017-03-28 14:50:28 +0900

    first commit

---
+++ instagram-clone/FilterCell.swift
@@ -0,0 +1,13 @@
+//
+//  FilterCell.swift
+//  instagram-clone
+//
+//  Created by Mac on 2017. 3. 28..
+//  Copyright © 2017년 huzzuni. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+}
